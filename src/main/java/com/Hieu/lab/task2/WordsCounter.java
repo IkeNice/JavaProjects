a357d2013f1034b177f38754ac31ff6b80e98c32
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-09-18 22:17:02 +0300

    second hw

--- src/main/java/com/Hieu/lab/task2/WordsCounter.java
+++ src/main/java/com/Hieu/lab/task2/WordsCounter.java
@@ -1,2 +1,21 @@
 package com.Hieu.lab.task2;
 
+
+public class WordsCounter{
+    private static String stri = "a aa cva a bv";
+    private static String simb = "a";
+    public static void main(String[] args) {
+        countWords(stri, simb);
+    }
+    public static int countWords(String str, String c)
+    {
+        int count = 0;
+        for (String st : str.split(" ")) {
+            if (c.equals(st)) {
+                count++;
+            }
+        }
+        System.out.println("Количество вхождений: " + count);
+        return 0;
+    }
+}
