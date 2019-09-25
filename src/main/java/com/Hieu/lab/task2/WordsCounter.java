c48ec990866e05568ad43237489dddda6bfcd397
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-09-25 22:28:49 +0300

    edit second hw

--- src/main/java/com/Hieu/lab/task2/WordsCounter.java
+++ src/main/java/com/Hieu/lab/task2/WordsCounter.java
@@ -1,21 +1,30 @@
 package com.Hieu.lab.task2;
 
-
 public class WordsCounter {
-    private static String stri = "a aa cva a bv";
-    private static String simb = "aa";
-    private static int countWords(String str, String c) {
+    private static String stri = "a aa bc vv cf";
+//    private static String simb = "aa";
+    private static void countWords(String str) {
         int count = 0;
-        for (String st : str.split(" ")) {
-            if (c.equals(st)) {
-                count++;
+        String[] part = str.split(" ");
+        int len = part.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (!part[i].equals(" ")) {
+                    if (part[i].equals(part[j])) {
+                        count++;
+                        part[j] = " ";
+                    }
+                }
             }
+            if (!part[i].equals(" "))
+                System.out.println(part[i] +" - " + (count+1));
+            count=0;
         }
-        System.out.println("Количество вхождений: " + count);
-        return 0;
+        if (!part[len-1].equals(" "))
+            System.out.println(part[len-1] +" - " + 1);
+    }
+    public static void main(String[] args) {
+        countWords(stri);
     }
 
-    public static void main(String[] args) {
-        countWords(stri, simb);
-    }
 }
