45e6ff5e09a19cd67e85a664bbc235c64a4e6c93
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-11-19 12:31:34 +0300

    world counter file

---
+++ src/main/java/com/Hieu/lab/task6/WCFile.java
@@ -0,0 +1,30 @@
+package com.Hieu.lab.task6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WCFile {
+    public static void WordsCounter(String fileName, Map<String, Integer> words) throws FileNotFoundException {
+        Scanner file = new Scanner(new File(fileName));
+        while (file.hasNext()){
+            String word = file.next();
+            Integer count = words.get(word);
+            if(count != null)
+                count++;
+            else
+                count =1;
+            words.put(word, count);
+        }
+        file.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> words = new HashMap<String, Integer>();
+        WordsCounter("C:\\Users\\hieut\\IdeaProjects\\lessonJava\\src\\main\\resources\\1.txt", words);
+        System.out.println(words);
+    }
+}
+
