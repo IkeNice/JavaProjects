80fd54ae67c51aa5af1fe7d9ae14a0822f897f4c
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-12-11 21:56:30 +0300

    can send a string

--- src/main/java/com/Hieu/lab/task7/TryToRunSpring.java
+++ src/main/java/com/Hieu/lab/task7/TryToRunSpring.java
@@ -2,15 +2,26 @@ package com.Hieu.lab.task7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
+
+@RestController
 @SpringBootApplication
 public class TryToRunSpring {
+
+    @RequestMapping("/test")
+    String page(){
+        return "Test string";
+    }
+
     public static void main(String[] args) {
 //        SpringApplication.run(TryToRunSpring.class, args); // использовать порт по умолчанию
+        // использовать указанный порт
         SpringApplication app = new SpringApplication(TryToRunSpring.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "8083")); //использовать указанный порт
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
         app.run(args);
     }
 }
