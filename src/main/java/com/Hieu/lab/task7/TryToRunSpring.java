718870579690d91328c02276d168be14ff5b1fbb
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-12-11 21:43:26 +0300

    change default port

--- src/main/java/com/Hieu/lab/task7/TryToRunSpring.java
+++ src/main/java/com/Hieu/lab/task7/TryToRunSpring.java
@@ -3,10 +3,14 @@ package com.Hieu.lab.task7;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class TryToRunSpring {
     public static void main(String[] args) {
-        SpringApplication.run(TryToRunSpring.class, args);
+//        SpringApplication.run(TryToRunSpring.class, args); // использовать порт по умолчанию
+        SpringApplication app = new SpringApplication(TryToRunSpring.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083")); //использовать указанный порт
+        app.run(args);
     }
-
 }
